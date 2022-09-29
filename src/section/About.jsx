d989ee2d3a0c604ddda010a101999e685fd52fae
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-09-29 03:17:01 -0300

    Cambio de estilos en los titulos

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -7,24 +7,20 @@ export const About = () => {
     <>
         <div className='bg-about' id='about'>
             <div className="section container-fluid animate__animated animate__fadeIn">
-
-                
-             
                 <Row >
-                    <Col  xs={24} lg={14} >
-                        
+                    <Col  xs={24} lg={14} >  
                         <div className='container-fluid img-flex'>
+                            
                             <picture className='img-curriculo'>
                                 <img src={img} />  
                             </picture>
-                            {/* <br />
-                            <p>Soy un estudiante de la carrera "Programador Universiatario" en la UNT. Mis mayores fortalezas son mi paciencia y mi empeño en aprender. Cada paso, aunque pequeño que sea, es un avance y eso me motiva a desempeñarme cada día mejor en mi disciplina. Soy amante de la tecnología, fotografías, de la naturaleza y de las reuniones con amigos.</p>               */}
                         </div>
                     </Col>
                     <Col xs={24} lg={10} >
-                        <div className='text-about'>                         
-                            <h2>Acerca de mi</h2> 
-                            <p>Hola!, mi nombre es Walter Daniel Carrizo y soy un estudiante de la carrera "Programador Universiatario" en la UNT. Mis mayores fortalezas son mi paciencia y mi empeño en aprender. Cada paso, aunque pequeño sea, es un avance y eso me motiva a desempeñarme cada día mejor en mi disciplina. Soy amante de la tecnología, fotografías y de la naturaleza. </p>
+                        <div className='text-about'>
+                                                  
+                             <h4>Hola! me llamo<br /><span>Walter Daniel Carrizo</span></h4>
+                            <p>Soy un estudiante de la tecnicatura "Programador Universiatario" en la Universidad Nacional de Tucumán, Argentina. Mis mayores fortalezas son mi paciencia y mi empeño en aprender. Cada paso, aunque pequeño sea, es un avance y eso me motiva a desempeñarme cada día mejor en mi disciplina. Soy amante de la tecnología, fotografías y de la naturaleza. </p>
                            
                         </div>
                     </Col>
